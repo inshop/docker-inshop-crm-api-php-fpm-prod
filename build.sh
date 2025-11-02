e4ec455fe11f457d1bbceca34307b1596ed6f605
Maksym Mukovoz <sgmaxim@gmail.com>
2025-11-02 10:56:08 +0100

    Upgrade PHP version to 8.4 in Dockerfile and build scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t inshopgroup/docker-inshop-crm-api-php-fpm-prod:php8.1 --push .
+docker build -t inshopgroup/docker-inshop-crm-api-php-fpm-prod:php8.4 --push .
